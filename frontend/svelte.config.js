de4ae7af9e3f7775dec6d17d06bc99fd5ffbde43
Reucov David <reucovd@gmail.com>
2025-06-27 12:02:02 +0200

    attempting to deploy to vercel

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
-
-export default config;
+export default {
+  kit: {
+    adapter: adapter({
+      out: 'build'
+    })
+  }
+};
